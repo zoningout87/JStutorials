ba7b069140b0df32015e575af3ac13c9f11802eb
Femke <femke@zoning-out.nl>
2023-09-06 15:19:35 +0200

    variabelen en value oefeningen

--- Javscript/script.js
+++ Javscript/script.js
@@ -1,5 +1,34 @@
 let js = "amazing";
-
-
-
 console.log (40 + 8 + 23 -10);
+
+
+//values 
+
+console.log ("Femke");
+console.log (23);
+
+//variabelen stored een waarde
+
+let firstName = "Femke";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Storyteller";
+
+console.log (myFirstJob);
+
+/* LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console*/
+
+let country = "The Netherlands";
+let continent = "Europe";
+let population = "20 million";
+
+console.log (country);
+console.log (continent);
+console.log (population);
