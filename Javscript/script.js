0ddb4d4f07c22772f1dbaa474f2125d97ff0589f
Femke <femke@zoning-out.nl>
2023-09-06 16:04:19 +0200

    extra oefeningen voor Datatypes

--- Javscript/script.js
+++ Javscript/script.js
@@ -25,15 +25,15 @@ console.log (myFirstJob);
 assign their values according to your own country (population in millions)
 2. Log their values to the console*/
 
-/*let country = "The Netherlands";
+let country = "The Netherlands";
 let continent = "Europe";
-let population = "20 million";
+let population = 20;
 
 console.log (country);
 console.log (continent);
-console.log (population);*/
+console.log (population);
 
-let javascriptIsFun = true;
+/*let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 console.log(typeof true);
@@ -49,4 +49,20 @@ console.log(year);
 console.log(typeof year);
 
 year = 1991;
-console.log(typeof year);
+console.log(typeof year);*/
+
+
+/*LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console*/
+
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
